d9f017f86c0d01a282e9ae6050f7bc8ea9d1bbad
zhongxm <zhongxiaoming@52tt.com>
2022-07-15 13:47:50 +0800

    v0.01

---
+++ ZXMPodAClassA.h
@@ -0,0 +1,20 @@
+//
+//  ZXMPodAClassA.h
+//  ZXMPodA
+//
+//  Created by samingzhong on 2022/7/15.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZXMPodAClassA : NSObject
+
+- (void)methodA;
+
+- (void)methodB;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ZXMPodAClassA.m
@@ -0,0 +1,15 @@
+//
+//  ZXMPodAClassA.m
+//  ZXMPodA
+//
+//  Created by samingzhong on 2022/7/15.
+//
+
+#import "ZXMPodAClassA.h"
+
+@implementation ZXMPodAClassA
+
+- (void)methodA {}
+- (void)methodB {}
+
+@end
